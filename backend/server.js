b6e017e15672ffe97d9838c1977d980e5f35c2b0
MrDracula7 <mrdracula534@gmail.com>
2026-01-03 15:59:31 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -17,3 +17,6 @@ mongoose.connect(process.env.MONGO_URI)
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
+
+const otpRoutes = require("./routes/otpRoutes");
+app.use("/api/otp", otpRoutes);
