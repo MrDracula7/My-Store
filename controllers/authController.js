9b0ddf8ede61fb26ef9f182f3d2ecd510d80cc14
MrDracula7 <mrdracula534@gmail.com>
2026-01-03 10:55:02 +0530

    Add files via upload

---
+++ controllers/authController.js
@@ -0,0 +1,37 @@
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+exports.signup = async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  try {
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword
+    });
+    res.json({ message: "User created" });
+  } catch {
+    res.status(400).json({ message: "User already exists" });
+  }
+};
+
+exports.login = async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+  if (!user) return res.status(404).json({ message: "User not found" });
+
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) return res.status(401).json({ message: "Wrong password" });
+
+  const token = jwt.sign(
+    { id: user._id, role: user.role },
+    process.env.JWT_SECRET
+  );
+
+  res.json({ token, user });
+};
