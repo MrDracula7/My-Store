53e7b3ac6e74c7c0617f521ffeaf433a6cf23453
MrDracula7 <mrdracula534@gmail.com>
2026-01-04 11:19:32 +0530

    Update otp.js

--- backend/models/otp.js
+++ backend/models/otp.js
@@ -1,9 +1,21 @@
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
+
+// const otpSchema = new mongoose.Schema({
+//   email: String,
+//   otp: String,
+//   expiresAt: Date
+// });
+
+// module.exports = mongoose.model("Otp", otpSchema);
+
+
+import mongoose from "mongoose";
 
 const otpSchema = new mongoose.Schema({
   email: String,
   otp: String,
+  verified: { type: Boolean, default: false },
   expiresAt: Date
 });
 
-module.exports = mongoose.model("Otp", otpSchema);
+export default mongoose.model("Otp", otpSchema);
