b2e5bba6550179c03280315f24d049fa95d4463b
MrDracula7 <mrdracula534@gmail.com>
2026-01-05 10:54:00 +0530

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,21 +1,12 @@
-// const mongoose = require("mongoose");
-
-// const userSchema = new mongoose.Schema({
-//   name: String,
-//   email: { type: String, unique: true },
-//   password: String,
-//   role: { type: String, default: "user" }
-// });
-
-// module.exports = mongoose.model("User", userSchema);
-
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
   name: String,
   email: { type: String, required: true, unique: true },
-  password: { type: String, required: true }
+  password: { type: String, required: true },
+  photo: { type: String } // profile image URL
 });
 
 module.exports = mongoose.model("User", userSchema);
+
 
