226075808839c22e2e869fa2624ab9e8db930098
MrDracula7 <mrdracula534@gmail.com>
2026-01-06 16:47:56 +0530

    Update authRoutes.js

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -5,7 +5,7 @@ const router = express.Router();
 router.post("/signup", signup);
 router.post("/login", login);
 
-module.exports = router;
+// module.exports = router;
 
 //dashboard route
 const auth = require("../middleware/authMiddleware");
@@ -40,6 +40,7 @@ module.exports = router;
 // });
 
 // module.exports = router;
+
 
 
 
