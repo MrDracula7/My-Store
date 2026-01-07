ae466091b82809a1414bec9c771a0ef2e84d6c6f
MrDracula7 <mrdracula534@gmail.com>
2026-01-07 10:37:00 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,38 +1,3 @@
-// const express = require("express");
-// const mongoose = require("mongoose");
-// const cors = require("cors");
-// require("dotenv").config();
-
-// const authRoutes = require("./routes/authRoutes");
-
-// const app = express();
-// app.use(cors({
-//   origin: "https://mrdracula7.github.io",
-//   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-//   credentials: true
-// }));
-// app.use(express.json());
-
-// app.use("/api/auth", authRoutes);
-
-// mongoose.connect(process.env.MONGO_URI)
-//   .then(() => console.log("MongoDB connected"))
-//   .catch(err => console.log(err));
-
-// const profileRoutes = require("./routes/profileRoutes");
-// app.use("/api/profile", profileRoutes);
-
-// app.use("/uploads", express.static("uploads"));
-
-// const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => console.log(`Server running on ${PORT}`));
-
-// const otpRoutes = require("./routes/otpRoutes");
-// app.use("/api/otp", otpRoutes);
-
-
-
-
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
@@ -40,7 +5,6 @@ require("dotenv").config();
 
 const authRoutes = require("./routes/authRoutes");
 const otpRoutes = require("./routes/otpRoutes");
-const profileRoutes = require("./routes/profileRoutes");
 
 const app = express();
 
@@ -54,7 +18,9 @@ app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/otp", otpRoutes);
-app.use("/api/profile", profileRoutes); // ✅ MUST be router
+
+app.use("/api/admin", require("./routes/admin"));
+app.post("/api/admin/login", require("./controllers/adminAuth").adminLogin);
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))
@@ -62,6 +28,7 @@ mongoose.connect(process.env.MONGO_URI)
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
+
 
 
 
