1382bf01daa24c149260739890d22ab6fc3f3f7c
MrDracula7 <mrdracula534@gmail.com>
2026-01-07 11:03:12 +0530

    Update createAdmin.js

--- backend/createAdmin.js
+++ backend/createAdmin.js
@@ -2,14 +2,15 @@ const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 const Admin = require("../models/Admin");
 
-mongoose.connect("mongodb://127.0.0.1:27017/yourDB");
+mongoose.connect("mongodb+srv://mystore:ol6zRPf6IZeO8ntp@cluster0.sshwz0c.mongodb.net/mystoredb?retryWrites=true&w=majority&authSource=admin");
 
 (async () => {
   const hashedPassword = await bcrypt.hash("admin123", 10);
 
   await Admin.create({
-    email: "admin@gmail.com",
-    password: hashedPassword
+    email: "mrdracula534@gmail.com",
+    password: $2b$10$iAfAZenFQF8UBCV2nIBzXe3dFzALCpXZkzdkESfKpmQJPLI0dcuWu,
+    role: "admin"
   });
 
   console.log("Admin created");
