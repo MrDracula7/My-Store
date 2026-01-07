7a82d2736cd5e3931054f534b5f306fc110c75a8
MrDracula7 <mrdracula534@gmail.com>
2026-01-07 18:34:34 +0530

    Update adminRoutes.js

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const router = express.Router();
 const adminAuth = require("../middleware/adminAuth");
+
 const {
   getAllUsers,
-  getUserCount
+  getUserCount,
+  toggleBlockUser,
+  deleteUser
 } = require("../controllers/adminUserController");
 
 router.get("/users", adminAuth, getAllUsers);
 router.get("/users/count", adminAuth, getUserCount);
+router.patch("/users/:id/block", adminAuth, toggleBlockUser);
+router.delete("/users/:id", adminAuth, deleteUser);
 
 module.exports = router;
