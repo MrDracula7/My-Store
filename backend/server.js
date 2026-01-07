07b04e17373c0d8ba5025ca2d42da0179ce002a0
MrDracula7 <mrdracula534@gmail.com>
2026-01-07 18:53:51 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(cors({
   origin: "https://mrdracula7.github.io",
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
   credentials: true
 }));
 
@@ -32,6 +32,7 @@ mongoose.connect(process.env.MONGO_URI)
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
+
 
 
 
