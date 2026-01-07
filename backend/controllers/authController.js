4c6f32b797230d95046befe5598210cecf323f52
MrDracula7 <mrdracula534@gmail.com>
2026-01-07 10:21:51 +0530

    Update authController.js

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -31,28 +31,6 @@ const signup = async (req, res) => {
   res.status(201).json({ message: "Account created successfully" });
 };
 
-// const login = async (req, res) => {
-//   const { email, password } = req.body;
-
-//   if (!email || !password) {
-//     return res.status(400).json({ message: "All fields required" });
-//   }
-
-//   const user = await User.findOne({ email });
-//   if (!user) {
-//     return res.status(401).json({ message: "Invalid credentials" });
-//   }
-
-//   const isMatch = await bcrypt.compare(password, user.password);
-//   if (!isMatch) {
-//     return res.status(401).json({ message: "Invalid credentials" });
-//   }
-
-//   res.json({ message: "Login successful" });
-// };
-
-// module.exports = { signup, login };
-
 
 const jwt = require("jsonwebtoken");
 
@@ -87,5 +65,6 @@ const login = async (req, res) => {
 };
 
 module.exports = { signup, login };
+
 
 
