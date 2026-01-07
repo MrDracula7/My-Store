077a4fd75f4b90ace3457c1738fab728adc5a284
MrDracula7 <mrdracula534@gmail.com>
2026-01-07 18:25:53 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -22,12 +22,17 @@ app.use("/api/otp", otpRoutes);
 app.use("/api/admin", require("./routes/admin"));
 app.post("/api/admin/login", require("./controllers/adminAuth").adminLogin);
 
+const adminRoutes = require("./routes/adminRoutes");
+app.use("/api/admin", adminRoutes);
+
+
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))
   .catch(err => console.log(err));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
+
 
 
 
