d56f55bedd9f5236c56fa2cd6085f378997ddd94
MrDracula7 <mrdracula534@gmail.com>
2026-01-07 18:33:44 +0530

    Update adminUserController.js

--- backend/controllers/adminUserController.js
+++ backend/controllers/adminUserController.js
@@ -1,11 +1,40 @@
 const User = require("../models/User");
 
+/* Get all users */
 exports.getAllUsers = async (req, res) => {
   const users = await User.find().select("-password");
   res.json(users);
 };
 
+/* Get user count */
 exports.getUserCount = async (req, res) => {
   const count = await User.countDocuments();
   res.json({ count });
 };
+
+/* Block / Unblock user */
+exports.toggleBlockUser = async (req, res) => {
+  const { id } = req.params;
+
+  const user = await User.findById(id);
+  if (!user) {
+    return res.status(404).json({ message: "User not found" });
+  }
+
+  user.isBlocked = !user.isBlocked;
+  await user.save();
+
+  res.json({
+    message: user.isBlocked ? "User blocked" : "User unblocked",
+    isBlocked: user.isBlocked
+  });
+};
+
+/* Delete user */
+exports.deleteUser = async (req, res) => {
+  const { id } = req.params;
+
+  await User.findByIdAndDelete(id);
+  res.json({ message: "User deleted" });
+};
+
