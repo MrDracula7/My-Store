0fd8c3612a6697267c4eac3a35492ebccf787c79
MrDracula7 <mrdracula534@gmail.com>
2026-01-07 10:23:46 +0530

    Update authRoutes.js

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -5,8 +5,6 @@ const router = express.Router();
 router.post("/signup", signup);
 router.post("/login", login);
 
-// module.exports = router;
-
 //dashboard route
 const auth = require("../middleware/authMiddleware");
 const User = require("../models/User");
@@ -18,28 +16,6 @@ router.get("/dashboard", auth, async (req, res) => {
 
 module.exports = router;
 
-// //profile route
-// const upload = require("../middleware/upload");
-
-// router.put("/profile", auth, upload.single("photo"), async (req, res) => {
-//   const { name } = req.body;
-
-//   const updateData = { name };
-
-//   if (req.file) {
-//     updateData.photo = `/uploads/${req.file.filename}`;
-//   }
-
-//   const user = await User.findByIdAndUpdate(
-//     req.user.id,
-//     updateData,
-//     { new: true }
-//   ).select("-password");
-
-//   res.json(user);
-// });
-
-// module.exports = router;
 
 
 
