c5f89e7a048f42fbe2188692734573ca017372d3
MrDracula7 <mrdracula534@gmail.com>
2026-01-08 10:54:43 +0530

    Create Product.js

---
+++ backend/models/Product.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  stock: {
+    type: Number,
+    default: 0
+  },
+  category: {
+    type: String
+  },
+  image: {
+    type: String
+  },
+  isActive: {
+    type: Boolean,
+    default: true
+  }
+}, { timestamps: true });
+
+module.exports = mongoose.model("Product", productSchema);
