6b74f6686190f7bdabcb347a8764c24607ddff39
MrDracula7 <mrdracula534@gmail.com>
2026-01-08 11:05:34 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ require("dotenv").config();
 
 const authRoutes = require("./routes/authRoutes");
 const otpRoutes = require("./routes/otpRoutes");
+const adminProductRoutes = require("./routes/adminProductRoutes");
 
 const app = express();
 
@@ -33,6 +34,7 @@ mongoose.connect(process.env.MONGO_URI)
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
+
 
 
 
