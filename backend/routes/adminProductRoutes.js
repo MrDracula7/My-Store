2c0a6bd68e000e9d5444ce93fe0d1d5ef4e7097b
MrDracula7 <mrdracula534@gmail.com>
2026-01-08 10:55:34 +0530

    Create adminProductRoutes.js

---
+++ backend/routes/adminProductRoutes.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const router = express.Router();
+const Product = require("../models/Product");
+const adminAuth = require("../middleware/adminAuth");
+
+// Get all products
+router.get("/", adminAuth, async (req, res) => {
+  const products = await Product.find();
+  res.json(products);
+});
+
+// Add product
+router.post("/", adminAuth, async (req, res) => {
+  const product = await Product.create(req.body);
+  res.json(product);
+});
+
+// Update product
+router.put("/:id", adminAuth, async (req, res) => {
+  const product = await Product.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    { new: true }
+  );
+  res.json(product);
+});
+
+// Delete product
+router.delete("/:id", adminAuth, async (req, res) => {
+  await Product.findByIdAndDelete(req.params.id);
+  res.json({ message: "Product deleted" });
+});
+
+// Activate / Deactivate
+router.patch("/:id/status", adminAuth, async (req, res) => {
+  const product = await Product.findById(req.params.id);
+  product.isActive = !product.isActive;
+  await product.save();
+  res.json(product);
+});
+
+module.exports = router;
