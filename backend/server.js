1c588e435014c087c9bd935e8dd5efe4ddd4a00f
MrDracula7 <mrdracula534@gmail.com>
2026-01-13 17:09:58 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -27,6 +27,9 @@ const adminRoutes = require("./routes/adminRoutes");
 app.use("/api/admin", adminRoutes);
 app.use("/api/admin/products", adminProductRoutes);
 
+app.use("/api/products", require("./routes/productRoutes"));
+
+
 app.use("/api/admin/categories", require("./routes/adminCategoryRoutes"));
 
 
@@ -36,6 +39,7 @@ mongoose.connect(process.env.MONGO_URI)
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
+
 
 
 
